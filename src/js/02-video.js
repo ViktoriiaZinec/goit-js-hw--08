babd48ef4d176a341896fad73aa78cce61f60298
ViktoriiaZinec <victoria.zinec@gmail.com>
2023-05-02 00:19:59 +0300

    Add video, feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const localStorageKey = 'videoplayer-current-time';
+
+player.on('loaded', () => {
+  const currentTime = localStorage.getItem(localStorageKey) || 0;
+  player.setCurrentTime(currentTime);
+});
+
+player.on(
+  'timeupdate',
+  _throttle(({ seconds }) => {
+    localStorage.setItem(localStorageKey, seconds);
+  }, 1000)
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const localStorageKey = 'feedback-form-state';
+
+form.addEventListener(
+  'input',
+  throttle(e => {
+    const objectToSave = { email: email.value, message: message.value };
+    localStorage.setItem(localStorageKey, JSON.stringify(objectToSave));
+  }, 500)
+);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all fields');
+  }
+
+  console.log({ email: email.value, message: message.value });
+  form.reset();
+  localStorage.removeItem(localStorageKey);
+});
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const storageData = load(localStorageKey);
+if (storageData) {
+  email.value = storageData.email;
+  message.value = storageData.message;
+}
